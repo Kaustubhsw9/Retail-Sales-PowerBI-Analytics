9eab83bc872906e3914631a4db0b3bab1ea1912a
Kaustubhsw9 <kaustubhwaghmare941999@gmail.com>
2026-06-26 18:26:26 +0530

    Update Business_Analysis.sql

--- SQL/Business_Analysis.sql
+++ SQL/Business_Analysis.sql
@@ -13,27 +13,27 @@ USE retail_sales_analysis;
 -- Section 1: Business KPIs
 -- =========================================================
 
--- Total Sales
+-- Query 1: Total Sales
 select round(sum(Sales),2) as Total_Sales
 from orders;
 
--- Total Profit 
+-- Query 2:  Total Profit 
 select round(sum(profit),2) as Total_Profit
 from orders;
 
--- Total Quality Sold 
-select sum(Quantity) as Total_Profit
+-- Query 3:  Total Quality Sold 
+select sum(Quantity) as Total_Quantity
 from orders;
 
--- Total Orders
+-- Query 4:  Total Orders
 select COUNT(DISTINCT `Order ID`) as Total_Orders
 from orders;
 
--- Total Customers
+-- Query 5:  Total Customers
 select COUNT(DISTINCT `Customer ID`) as Total_Customers
 from orders;
 
--- Total Products 
+-- Query 6:  Total Products 
 select count(distinct `Product ID`) as Total_Products
 from orders;
 
@@ -41,7 +41,7 @@ from orders;
 --section2: Customer Analysis
 -- ============================================================
 
--- Sales, Profit & Quantity by Segment
+-- Query 6:  Sales, Profit & Quantity by Segment
 SELECT
     Segment,
     ROUND(SUM(Sales),2) AS Total_Sales,
@@ -56,47 +56,40 @@ ORDER BY Total_Sales DESC;
 -- section 3: Geographic  Analysis
 -- =============================================================
 
--- country-wise Sales & Profit
-
-
+-- Query 7:  country-wise Sales & Profit
 select 
-Country,
-round(sum(Sales),2) as Total_Sales,
-round(sum(Profit),2) as Total_Profit
-
+    Country,
+    round(sum(Sales),2) as Total_Sales,
+    round(sum(Profit),2) as Total_Profit
 from orders
 group by Country 
 order by Total_Sales desc;
 
--- State-wise Sales
+-- Query 8:  State-wise Sales
 
 select 
-State,
-round(sum(sales),2) as Total_sales
-
+    State,
+    round(sum(sales),2) as Total_sales
 from orders
 group by State
 order by Total_Sales desc;
 
--- State-wise Profit
+-- Query 9:  State-wise Profit
 use retail_sales_analysis;
 select 
-state,
-round(sum(Profit),2) as Total_Profit
-
+    state,
+    round(sum(Profit),2) as Total_Profit
 from orders
 group by State
 order by Total_Profit desc;
 
--- Top 10 Cities By Sales 
-
+-- Query 10:  Top 10 Cities By Sales 
 
 select 
-City,
-round(sum(Sales),2) as Total_Sales
+    City,
+    round(sum(Sales),2) as Total_Sales
 
 from orders
-
 group by City
 order by Total_Sales desc
 limit 10;
